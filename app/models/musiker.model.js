c4b0ec4ff9cd1d7bb017898ff914261a2eaa6ab0
GTDominic <seitheldominic@gmail.com>
2020-05-11 13:43:22 +0200

    removed unnecassary code

--- app/models/musiker.model.js
+++ app/models/musiker.model.js
@@ -1,24 +1,3 @@
-// module.exports = (sequelize, Sequelize) => {
-//     const Musiker = sequelize.define("musiker", {
-//       name: {
-//           type: Sequelize.STRING
-//       }
-//     }),
-//     Instrument = sequelize.define("musiker", {
-//         name: {
-//             type: Sequelize.STRING
-//         }
-//     });
-    
-//     Musiker.belongsTo(Instrument);
-      
-//     Instrument.hasMany(Musiker, {
-//         foreignKey: 'instrumentid'
-//     });
-
-//     return Musiker;
-//   };
-
 'use strict';
 module.exports = (sequelize, DataTypes) => {
     var Musiker = sequelize.define('Musiker', {
